bace9a9221cdb6fc85158e2b8b1b1c9ae3b457a3
Jasmine Quach <xjasminequach@gmail.com>
2018-08-17 10:46:47 -0500

    Update methods to render paragraph array

--- app/controllers/ipsum_controller.rb
+++ app/controllers/ipsum_controller.rb
@@ -1,20 +1,25 @@
 class IpsumController < ApplicationController
   def index
-  end
-
-  def show
-
-  end
-
-  def new
-    @ipsum = Ipsum.new
+    @paragraph_arr ||= []
   end
 
   def generate
-    get_words(params[:num])
+    words = get_words(params[:num])
+
+    if words.present?
+      render :index
+    end
+
+    # if @ipsum.valid?
+    #   render json: @ipsum
+    # else
+    #   render json: @ipsum.errors, status: :unprocessable_entity
+    # end
+    # @ipsum.save
+    # render plain: params[:model].inspect
   end
 
-  private
+  # private
   def get_words(num)
     rand_range = rand(8..12).to_i
     rand_sentences = rand(4..6).to_i
@@ -34,7 +39,6 @@ class IpsumController < ApplicationController
       @paragraph_arr << @sentence_arr.slice!(0, rand(4..6).to_i).join(' ')
     end
 
-    # @paragraph_arr.map!{|p| p << '\n'}
-    # binding.pry
+    @paragraph_arr
   end
 end
