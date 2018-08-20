ca62bd0c58d4281d18409f6afdd8c426c375b2af
Jasmine Quach <xjasminequach@gmail.com>
2018-08-20 15:28:16 -0500

    Refactor into several methods and succesfully working

--- app/controllers/ipsum_controller.rb
+++ app/controllers/ipsum_controller.rb
@@ -5,35 +5,34 @@ class IpsumController < ApplicationController
 
   def generate
     words = get_words(params[:num])
+    sentences = get_sentences(words)
+    paragraphs = get_paragraphs(sentences)
 
-    if words.present?
+    if paragraphs.present?
       render :index
     end
-
-    # if @ipsum.valid?
-    #   render json: @ipsum
-    # else
-    #   render json: @ipsum.errors, status: :unprocessable_entity
-    # end
-    # @ipsum.save
-    # render plain: params[:model].inspect
   end
 
-  # private
+  private
   def get_words(num)
     rand_range = rand(8..12).to_i
     rand_sentences = rand(4..6).to_i
     random_num = rand_range * rand_sentences * num.to_i
     word_arr = Ipsum.limit(random_num).pluck('word')
+  end
 
+  def get_sentences(word_arr)
     @sentence_arr = []
-    @paragraph_arr = []
 
     while word_arr.count > 0
       @sentence_arr << word_arr.slice!(0, rand(8..12).to_i).join(' ')
     end
 
     @sentence_arr.map!{|s| s.capitalize << '.'}
+  end
+
+  def get_paragraphs(sentence_arr)
+    @paragraph_arr = []
 
     while @sentence_arr.count > 0
       @paragraph_arr << @sentence_arr.slice!(0, rand(4..6).to_i).join(' ')
