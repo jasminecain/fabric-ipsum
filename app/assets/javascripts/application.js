b7f81ca8fa5707acd4ab790abdbf906e105bfaea
Jasmine Quach <xjasminequach@gmail.com>
2018-08-21 14:34:56 -0500

    Add Gems for clipboard and jquery. Also including reference files to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,16 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+// = require rails-ujs
 //= require turbolinks
 //= require_tree .
+//= require clipboard
+//= require jquery
+//= require jquery_ujs
+
+$(function () {
+
+  var clipboard = new Clipboard('.clipboard-btn');
+  console.log(clipboard);
+
+});
